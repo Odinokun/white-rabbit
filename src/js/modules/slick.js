cb83684bff281305c28ca826fab2dd1b1ef9f95c
odinokun <odinokun@gmail.com>
2019-11-07 12:08:51 +0200

    did cases page

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -22,6 +22,40 @@ module.exports = function () {
 
   });
 
+  $('.cases-slider').slick({
+    infinite: true,
+    dots:false,
+    arrows:false,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    draggable: true,
+    touchMove: true,
+    swipe: true,
+    touchThreshold: 100,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 981,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  const slider = $(".cases-slider");
+
+  slider.on("wheel", function(e) {
+    e.preventDefault();
+
+    if (e.originalEvent.deltaY < 0) {
+      $(this).slick("slickNext");
+    } else {
+      $(this).slick("slickPrev");
+    }
+  });
+
   // end Slick slider
 
 };
